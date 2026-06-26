e55c5768794c0ccd113787f6fef6f7acfd72b6dd
RupDey <rupdey2006@gmail.com>
2026-06-26 20:12:02 +0530

    Update Attendence.sql

--- Attendence.sql
+++ Attendence.sql
@@ -1,20 +1,17 @@
 USE attendance_system;
-SELECT * FROM attendance_system.Attendence;
+SELECT * FROM Attendance;
 
-CREATE TABLE Attendence (
 
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(100),
-mobile VARCHAR(15),
-email VARCHAR(100),
-roll VARCHAR(20),
-father VARCHAR(100),
-mother VARCHAR(100),
-course VARCHAR(50),
-dob DATE,
-address TEXT
-
+CREATE TABLE Attendance (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    mobile_number VARCHAR(15),
+    email VARCHAR(100),
+    roll VARCHAR(20),
+    father VARCHAR(100),
+    mother VARCHAR(100),
+    course VARCHAR(50),
+    dob DATE,
+    address TEXT
 );
-
-
 
